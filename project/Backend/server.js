a89aab20f9d60a5ea31ee4c9ea855eb14706bdb4
Ritana . <ksenja.vassiljeva@ivkhk.ee>
2026-04-07 01:46:11 +0300

    update files server and packeg

--- project/Backend/server.js
+++ project/Backend/server.js
@@ -1,24 +1,30 @@
-import express from "express";
-import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// Import Section
+const cors = require("cors");
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// App use section
 app.use(cors());
 app.use(express.json());
+
 
 app.use(express.static(path.resolve(__dirname, "../Frontend")));
 app.use(express.static(path.resolve(__dirname, "../Frontend/scripts")));
 
+// Главная страница
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../Frontend/mainPage/index.html"));
+    try {
+        res.sendFile(path.resolve(__dirname, "../Frontend/mainPage/index.html"));
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: e.message });
+    }
 });
 
+// Listener
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
 });

90a883f8630287e89e896f285b2f4e7e150c788b
ksenjavassiljeva <ksenja.vassiljeva@ivkhk.ee>
2026-04-14 09:28:24 +0300

    update middleware

--- project/public/middleware/auth.middleware.js
+++ project/public/middleware/auth.middleware.js
@@ -3,23 +3,24 @@ import { createPB } from "../config/pocketbase.js";
 export const authMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader?.startsWith("Bearer ")) {
-    return res.status(401).json({ error: "No token provided" });
+  if (!authHeader) {
+    return res.status(401).json({ error: "No token" });
   }
 
   const token = authHeader.split(" ")[1];
 
-  try {
-    const pb = createPB();
-    pb.authStore.save(token, null);
+  const pb = createPB();
 
+  // ВАЖНО: подставляем токен
+  pb.authStore.save(token, null);
+
+  try {
+    // Проверяем токен
     await pb.collection("users").authRefresh();
 
-    req.user = pb.authStore.model;
     req.pb = pb;
-
     next();
-  } catch {
-    res.status(401).json({ error: "Invalid or expired token" });
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token" });
   }
 };
